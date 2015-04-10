82c3c93647b2ce60e621a6937c3f53ab12b15cf2
Damian Reeves <damreev@gmail.com>
2015-04-10 09:46:28 -0400

    Save this and moving to another machine

--- src/Kollege.Server/EventStoreHosting.fs
+++ src/Kollege.Server/EventStoreHosting.fs
@@ -9,29 +9,46 @@ type EmbeddedEventStore = {
       Connection: IEventStoreConnection;
 }
 
-
 module EventStoreFacade =
+  open System
   open System.Net
+  open System.IO
   open EventStore.Core
   open EventStore.ClientAPI.Embedded
   open EventStore.ClientAPI  
 
-  let get () =
+  let start (configure:EmbeddedVNodeBuilder->EmbeddedVNodeBuilder) =
     let noIp = new IPEndPoint(IPAddress.None, 0)
+    let dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db")
+
+    let build (builder:EmbeddedVNodeBuilder) =
+
+      
+      match Directory.Exists(dbPath) with
+      | false -> Directory.CreateDirectory(dbPath)|>ignore
+      | true -> ()
+
+      builder.Build()
+
     let vnode = 
         EmbeddedVNodeBuilder
             .AsSingleNode()
-            .RunInMemory()
-            .WithExternalTcpOn(noIp)
-            .WithInternalTcpOn(noIp)
-            .WithExternalHttpOn(noIp)
-            .WithInternalHttpOn(noIp)
-            .Build()
+            //.RunInMemory()
+            .RunOnDisk(dbPath)
+            .OnDefaultEndpoints()
+//            .WithExternalTcpOn(noIp)
+//            .WithInternalTcpOn(noIp)
+//            .WithExternalHttpOn(noIp)
+//            .WithInternalHttpOn(noIp) 
+            |> configure  
+            |> build
+
     vnode.Start()
     let connection = EmbeddedEventStoreConnection.Create(vnode)
-    {VNode = vnode; Connection = connection;}
+    {VNode = vnode; Connection = connection;}    
+  
+  let get () = start (fun x -> x)
 
   let stop es =
       es.Connection.Close()
-      es.VNode.Stop()
 
