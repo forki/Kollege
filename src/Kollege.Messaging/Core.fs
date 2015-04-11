ddbd4cd44855803bf685995388052eb2c0cf6168
DamianReeves <DamReev@gmail.com>
2015-04-10 22:37:39 -0400

    more stuff

---
+++ src/Kollege.Messaging/Core.fs
@@ -0,0 +1,9 @@
+﻿namespace Kollege.Messaging
+
+open System
+
+type IPublisher = 
+    abstract member Publish : message:'TMessage -> unit
+
+type ISubscriber =
+  abstract member Subscribe : onMessage:Action<'TMessage> -> unit
